8cede224aa329b7da0d954e369ca04357e523ac0
Diego HC <amerika_hu_cob@hotmail.com>
2023-11-15 16:55:37 -0600

    creando formato de fechas y anexando nueva url

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from  .views import renderPosts, post_detail
 
+app_name = 'blog'
 urlpatterns = [
     path("", renderPosts, name="posts"),
-    path("<int:post_id>", post_detail),
+    path("<int:post_id>", post_detail, name ='post_detail')  ,
 ]
