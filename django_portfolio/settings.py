8b5d249914ca8a0a144b55ca6206376aa0fa5c20
Diego HC <amerika_hu_cob@hotmail.com>
2023-11-15 13:20:35 -0600

    solucion de carga de imagenes

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = BASE_DIR / 'media' 
+MEDIA_ROOT = BASE_DIR / 'media' #concentrar todas las imagens en una carpeta media.
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/' #hacer publica la carpeta
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
